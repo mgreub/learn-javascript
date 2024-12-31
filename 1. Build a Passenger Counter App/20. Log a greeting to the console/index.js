99a9e5c69493cb94183160f50cd08131717994ca
mgreub <145691444+mgreub@users.noreply.github.com>
2024-12-31 09:58:20 +0100

    Update index.js

--- 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
+++ 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
@@ -1,7 +1,11 @@
 // Create two variables, name and greeting. The name variable should store your name,
 // and the greeting should store e.g. "Hi, my name is "
 
+let name = "Marius"
+let greeting = "Hi, my name is"
+
 // Create a third variable, myGreeting, that contatenates the two strings
+let myGreeting = 
 // Log myGreeting to the console
 
-
+console.log (name)
